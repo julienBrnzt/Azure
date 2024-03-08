fd855dbbc44d0ffc0e74654d04b42289187a66c6
julienBrnzt <julienkorvers@hotmail.fr>
2024-03-08 14:32:06 +0100

    Start function , Stop function

---
+++ StopTrigger/run.ps1
@@ -0,0 +1,72 @@
+#// code/vmPowerFunction.ps1
+
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($request, $TriggerMetadata)
+
+#Set default value
+$status = 200
+
+Try{
+    # Write to the Azure Functions log stream.
+    Write-Output "PowerShell HTTP trigger function processed a request."
+
+    # Interact with query parameters or the body of the request.
+    Write-Output ($request | ConvertTo-Json -depth 99)
+
+    $ResourceGroupName = "ResourceGroup"
+    $VMName1 = "VM01"
+    $VMName2 = "VM02"
+    $Context =  "1d47430d-fbbf-4a50-8212-436a2ad92276"
+
+    $null = Connect-AzAccount -Identity
+    $null = Set-AzContext $Context
+
+    $vmStatus1 = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName1 -Status
+    Write-output $vmStatus1
+    If(-not ($vmStatus1)){
+        $status = 404
+        Throw "ERROR! VM [$VMName1] not found. Please check if the 'Subscription ID', 'Resource Group Name' or 'VM name' is correct and exists."
+    }
+    [string]$Message = "Virtual machine [$VMName1] status: " + $vmStatus1.statuses[-1].displayStatus
+       
+    If($vmStatus1.statuses[-1].displayStatus -ne 'VM deallocated'){
+        Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName1 -Force -Verbose
+        [string]$message += "... Virtual machine [$VMName1] is stopping"
+    }
+    Else{ 
+        [string]$message += "... Virtual machine [$VMName1] is already deallocated"
+    }
+
+    $vmStatus2 = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName2 -Status
+    Write-output $vmStatus2
+    If(-not ($vmStatus2)){
+        $status = 404
+        Throw "ERROR! VM [$VMName2] not found. Please check if the 'Subscription ID', 'Resource Group Name' or 'VM name' is correct and exists."
+    }
+    [string]$Message = "Virtual machine [$VMName2] status: " + $vmStatus2.statuses[-1].displayStatus
+       
+    If($vmStatus2.statuses[-1].displayStatus -ne 'VM deallocated'){
+        Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName2 -Force -Verbose
+        [string]$message += "... Virtual machine [$VMName2] is stopping"
+    }
+    Else{ 
+        [string]$message += "... Virtual machine [$VMName2] is already deallocated"
+    }
+     
+}
+Catch{
+    [string]$message += $_
+}
+
+Write-output $message
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value (
+    [HttpResponseContext]@{
+        StatusCode = $status
+        body = [string]$message
+        headers = @{ "content-type" = "text/plain" }
+    }
+)
